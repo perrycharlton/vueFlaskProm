fdffdbdb21c4a67753a9621746bf2f49bf054d18
perry <perry@perrycharlton.com>
2017-10-21 14:38:00 +0100

    updated content

--- static/js/student_form.js
+++
@@ -1,8 +0,0 @@
-let lbl = document.createElement('label');
-let input = document.createElement('input');
-let btn = document.createElement('button');
-let form = document.createElement('form');
-let div = document.createElement('div');
-form.className = 'form-inline';
-div.className = 'form-group';
-lbl.className = 'sr-only';
